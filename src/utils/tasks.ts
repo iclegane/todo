6ad10e29a999a6a3281b8c3c37696f55470e6b07
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-29 12:10:40 +0300

    added mock task array

---
+++ src/utils/tasks.ts
@@ -0,0 +1,14 @@
+import { ITask } from "../types/task";
+
+export const tasks: ITask[] = [
+    { name: 'Task 1' },
+    { name: 'Task 2' },
+    { name: 'Task 3' },
+    { name: 'Task 4' },
+    { name: 'Task 5' },
+    { name: 'Task 6' },
+    { name: 'Task 7' },
+    { name: 'Task 8' },
+    { name: 'Task 9' },
+    { name: 'Task 10' },
+];
