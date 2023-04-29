68c041962f07694037be8e3692dd6b4247d99fa6
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-29 12:10:05 +0300

    added default styles

---
+++ src/css/css.tsx
@@ -0,0 +1,12 @@
+import styled from "@emotion/styled";
+
+export const AppContainer = styled.div(({
+    position: 'relative',
+    width: '100%',
+    height: '100%'
+}));
+
+export const FlexColumn = styled.div(({
+    display: 'flex',
+    flexDirection: 'column'
+}));
