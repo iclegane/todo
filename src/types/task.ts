fe8c1dbf8f557fa409294769cd3e059f55ee30bb
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-29 18:55:44 +0300

    added task status

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,3 +1,4 @@
 export interface ITask {
     name: string;
+    status: boolean;
 }
--- src/utils/tasks.ts
+++ src/utils/tasks.ts
@@ -1,14 +1,14 @@
 import { ITask } from "../types/task";
 
 export const tasks: ITask[] = [
-    { name: 'Task 1' },
-    { name: 'Task 2' },
-    { name: 'Task 3' },
-    { name: 'Task 4' },
-    { name: 'Task 5' },
-    { name: 'Task 6' },
-    { name: 'Task 7' },
-    { name: 'Task 8' },
-    { name: 'Task 9' },
-    { name: 'Task 10' },
+    { name: 'Task 1', status: false },
+    { name: 'Task 2', status: false },
+    { name: 'Task 3', status: false },
+    { name: 'Task 4', status: false },
+    { name: 'Task 5', status: false },
+    { name: 'Task 6', status: false },
+    { name: 'Task 7', status: false },
+    { name: 'Task 8', status: false },
+    { name: 'Task 9', status: true },
+    { name: 'Task 10', status: false },
 ];
