84562ba5433b2ad0032cf9d14032285a901723bb
Viktor <vvs-PathOfExile@yandex.ru>
2023-04-29 20:59:06 +0300

    added onChangeTask

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -2,14 +2,21 @@ import { FlexColumn } from '../../css/css';
 import Task from '../Task/Task';
 import { ITaskListProps } from './types';
  
-const TaskList: React.FC<ITaskListProps> = ({ items = [] }) => {
+const TaskList: React.FC<ITaskListProps> = ({ items = [], onChangeTask }) => {
     if (!items.length) {
         return null;
     }
 
     return (
         <FlexColumn>
-            { items.map((task) => <Task key={ task.name } {...task} /> )}
+            {items.map((task, index) => 
+                <Task 
+                    key={ task.name }
+                    name={ task.name }
+                    status={ task.status }
+                    onChangeTask={(newValue) => onChangeTask(index, newValue)}
+                /> 
+            )}
         </FlexColumn>
     )
 }
